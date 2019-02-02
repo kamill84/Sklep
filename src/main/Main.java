001e9c3ee189bf2d33d1344c0d1ea6103199da75
kamill84 <kamill84@o2.pl>
2019-02-02 10:44:48 +0100

    Sklep

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import ui.Menu;
+
+import java.awt.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        ui.Menu menuAplikacji = new Menu();
+        menuAplikacji.wyswietl();
+
+    }
+}
---
+++ src/ui/Menu.java
@@ -0,0 +1,60 @@
+package ui;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public void wyswietl(){
+        Scanner wejscie = new Scanner(System.in);
+
+
+        System.out.println("1 - dodaj akohol ");
+        System.out.println("2 - usun alkohol ");
+        System.out.println("3 - wyswietl ");
+        System.out.println("q - wyjscie ");
+
+
+
+
+
+        String text="";
+
+            // porownanie string przez equals
+
+
+        while (!text.equals("q")){
+
+            System.out.print("Wybierz : ");
+            text = wejscie.nextLine();
+
+            if (text.equals("1") ) {
+                System.out.println("Dodaj");
+            }
+
+
+            if (text.equals("2"))  {
+                System.out.println("Usun");
+            }
+
+
+            if ( text.equals("3")) {
+                System.out.println("Wyswietl");
+            }
+
+
+            if ( text.equals("q")) {
+                System.out.println("Koniec programu ");
+            }
+
+
+            if ( !text.equals("1") && !text.equals("2") &&  !text.equals("3") && !text.equals("q")  ) {
+                System.out.println("Nieznana komenda ");
+            }
+
+        }
+    }
+
+}
+
+
+
